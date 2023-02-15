0f300e0510aa541cf03df1d9cf06381994b707bb
Jared Staples <jstaplesmusic@gmail.com>
2023-02-14 21:41:06 -0500

    feature(Homepage.js): created basic component that fetches data from database and renders it in browser

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ALL_PRODUCTS } from "../utils/queries";
+
+const homePageStyle = {
+  width: "100%",
+  height: "100vh",
+  background: "#14080E",
+  overflowY: "scroll",
+};
 
 const Homepage = () => {
-  return <div>Homepage Component</div>;
+  const { data } = useQuery(QUERY_ALL_PRODUCTS);
+  console.log(data);
+  const products = data?.products || [];
+
+  return (
+    <div style={homePageStyle}>
+      <h1>Homepage Component</h1>
+      {products.map((product) => (
+        <div>
+          <li key={product._id}>{product.name}</li>
+          <li key={product.name}>{product.description}</li>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Homepage;
