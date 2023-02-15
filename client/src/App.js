f6c953f62be6af0879bc6a7ffa715f56a263a831
Charlotte Murphey <murphey.charlotte@gmail.com>
2023-02-14 19:59:21 -0500

    edited Home title in Routes to Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,7 @@ function App() {
             <Routes>
               <Route 
                 path="/"
-                element={<Home />}
+                element={<Homepage />}
               />
               <Route 
                 path="/login"
