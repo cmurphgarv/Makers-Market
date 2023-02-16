596802bddae491b25b5dca2535ef4d6804008033
Jared Staples <jstaplesmusic@gmail.com>
2023-02-16 18:50:57 -0500

    Created button on homepage that renders if user has a valid token

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
 const ProductItem = ({ name, image, price, _id }) => {
   return (
-    <div class="">
-      <div class="productItem">
+    <div className="">
+      <div className="productItem">
         <Link to={`/products/${_id}`}>
           <img src={`/images/${image}`} alt={name} />
-          <h5>{name}</h5>${price}
+          <h5>{name}</h5>
+          <p>${price}</p>
         </Link>
+        {Auth.loggedIn() && <button>Add to Cart</button>}
       </div>
     </div>
   );
