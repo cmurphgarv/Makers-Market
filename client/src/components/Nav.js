58fcb5ca3be7efbf9c89702ded48df3adcf4c1d8
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2023-02-21 20:27:57 -0500

    Added history link and made cart viewable only when logged in.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
 let loginButton;
+let cartButton;
+let historyButton;
 
 const handleLogOut = () => {
   Auth.logout();
-}
+};
 
 if (Auth.loggedIn()) {
   loginButton = (
@@ -17,9 +19,27 @@ if (Auth.loggedIn()) {
 } else {
   loginButton = (
     <Link to={`/login`}>
-      <h5>Login/Sign-Up</h5>
+      <h5>Login/Sign Up</h5>
     </Link>
   );
+}
+if (Auth.loggedIn()) {
+  cartButton = (
+    <Link to={`/cart`}>
+      <h5>View Cart</h5>
+    </Link>
+  );
+} else {
+  cartButton = "";
+}
+if (Auth.loggedIn()) {
+  historyButton = (
+    <Link to={`/history`}>
+      <h5>View Order Histroy</h5>
+    </Link>
+  );
+} else {
+  historyButton = "";
 }
 
 const Nav = () => {
@@ -34,12 +54,9 @@ const Nav = () => {
         </div>
         <div class="navlogin">
           <ul>
+            <li>{historyButton}</li>
+            <li>{cartButton}</li>
             <li>{loginButton}</li>
-            <li>
-              <Link to={`/cart`}>
-                <h5>View Cart</h5>
-              </Link>
-            </li>
           </ul>
         </div>
       </div>
